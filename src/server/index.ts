9b0c0bb1e80a953f99ad88088401ec040a447c84
Ashish Mohapatra <ashishmohapatra240@gmail.com>
2025-08-29 23:52:00 +0530

    feat: add routes for candles and trades

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from "express";
 import authRouter from "./routes/auth.route";
+import candlesRouter from "./routes/candles.route";
+import orderRouter from "./routes/order.route";
 
 const app = express();
 
@@ -11,6 +13,9 @@ app.get("/", (req: Request, res: Response) => {
 });
 
 app.use("/auth", authRouter);
+app.use("/candles", candlesRouter);
+app.use("/trades", orderRouter);
+
 const port = Number(process.env.PORT) || 3000;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
