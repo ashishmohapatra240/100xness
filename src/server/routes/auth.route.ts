02111d2d869fb47ebbce94d15e0990563d7696d2
Ashish Mohapatra <ashishmohapatra240@gmail.com>
2025-09-02 11:42:29 +0530

    feat: add user retrieval endpoint and authentication middleware to auth routes

--- src/server/routes/auth.route.ts
+++ src/server/routes/auth.route.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import { login, logout, register } from "../controller/auth.controller";
+import { getUser, login, logout, register } from "../controller/auth.controller";
+import { authenticate } from "../middleware/authenticate";
 
 const router: Router = Router();
 
 router.post("/register", register);
 router.post("/login", login);
+router.post("/me", authenticate, getUser);
 router.post("/logout", logout);
 
 export default router;
